6bc7b0b64a54dd53ea220e3c4692649b056f8798
Andy Tuttle <andytuttle12@gmail.com>
2017-09-07 16:02:41 -0400

    Added sign-in functionality with jwt from cloud functions.

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyCDMEg4L4qri_pfC5mQerkwmGSATLkTEvU",
+      authDomain: "one-time-password-auth.firebaseapp.com",
+      databaseURL: "https://one-time-password-auth.firebaseio.com",
+      projectId: "one-time-password-auth",
+      storageBucket: "one-time-password-auth.appspot.com",
+      messagingSenderId: "504640557490"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -17,6 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
